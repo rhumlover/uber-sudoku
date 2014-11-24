0621f3af7cdf955795ace4c5705668141e7056c7
Thomas Le Jeune <thomas.lejeune@dailymotion.com>
2014-11-24 10:10:08 -0800

    Break the big constructor code into more reuseable methods. Give the possibility to assign a Component to a new DOM node

--- src/js/lib/ui-component.js
+++ src/js/lib/ui-component.js
@@ -4,55 +4,78 @@
  * the main Facade, wich contains the Game/Domain logic.
  */
 function UIComponent(options) {
-  var self = this,
-    rootEl = options.el;
+  var self = this;
+
+  this.el = null;
+  this.domEvents = {};
+  this.externalEvents = {};
 
   _.extend(this, options);
 
-  if (!rootEl) {
-    rootEl = document.createElement('div');
-  }
-  else if (_.isString(rootEl)) {
-    rootEl = document.querySelector(rootEl);
-  }
-
-  this.el = rootEl;
-  this.$el = $(rootEl);
-  this.active = !this.$el.hasClass('is-inactive');
+  // Root DOM Element
+  this.assignTo(options.el);
 
   // DOM events
-  (function(events) {
-    if (!events) return;
-
-    _.forEach(events, function(callback, key) {
-      var firstSpace = key.indexOf(' '),
-        eventType = key.slice(0, firstSpace),
-        selector = key.slice(firstSpace);
-
-      if (_.isString(callback)) {
-        callback = self[callback];
-      }
-
-      self.$el.on(eventType, selector, callback.bind(self));
-    });
-  })(options.domEvents);
+  this.listenDomEvents(options.domEvents);
 
   // External events (from PubSub)
-  (function(events) {
-    if (!events) return;
-
-    _.forEach(events, function(callback, key) {
-      if (_.isString(callback)) {
-        callback = self[callback];
-      }
-      PubSub.subscribe(key, callback.bind(self));
-    });
-  })(options.externalEvents);
+  this.listenExternalEvents(options.externalEvents);
 
   if (_.isFunction(options.init)) {
     options.init.call(this);
   }
 }
+
+UIComponent.prototype.assignTo = function(elt) {
+  try {
+    if (!elt) {
+      throw "UIComponent: no root element passed in options";
+    }
+    else if (_.isString(elt)) {
+      elt = document.querySelector(elt);
+      if (!elt) {
+        throw "UIComponent: no DOM Node matches the given selector (" + options.el + ")";
+      }
+    }
+    else if (!('nodeType' in elt) || elt.nodeType !== elt.ELEMENT_NODE) {
+      throw "UIComponent: passed element isn't a DOM Node";
+    }
+  }
+  catch (e) {
+    elt = document.createElement('div');
+  }
+  this.el = elt;
+  this.$el = $(elt);
+  this.active = !this.$el.hasClass('is-inactive');
+};
+
+UIComponent.prototype.listenDomEvents = function(events) {
+  if (_.isEmpty(events)) return;
+  var self = this;
+
+  _.forEach(events, function(callback, key) {
+    var firstSpace = key.indexOf(' '),
+      eventType = key.slice(0, firstSpace),
+      selector = key.slice(firstSpace);
+
+    if (_.isString(callback)) {
+      callback = self[callback];
+    }
+    self.$el.on(eventType, selector, callback.bind(self));
+  });
+};
+
+UIComponent.prototype.listenExternalEvents = function(events) {
+  if (_.isEmpty(events)) return;
+  var self = this;
+
+  _.forEach(events, function(callback, key) {
+    if (_.isString(callback)) {
+      callback = self[callback];
+    }
+    PubSub.subscribe(key, callback.bind(self));
+  });
+};
 
 // UI helpers: activate/deactivate element following the css convention
 // that reacts to the presence of 'is-inactive' class
